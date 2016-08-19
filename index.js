311942e38d36ea9bf96a5238174a8155437b62be
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-08-19 11:17:22 +0200

    Adss test files. Fixes methods

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 'use strict'
 
+const miss = require('mississippi')
+
+const fromString = string => {
+  return miss.from(function (size, next) {
+    // if there's no more content
+    // left in the string, close the stream.
+    if (string.length <= 0) return next(null, null)
+
+    // Pull in a new chunk of text,
+    // removing it from the string.
+    var chunk = string.slice(0, size)
+    string = string.slice(size)
+
+    // Emit "chunk" from the stream.
+    next(null, chunk)
+  })
+}
+
 module.exports = (item, callback) => {
-  const miss = require('mississippi')
   const getNextJob = require('./lib/get-next-job')
   const saveJobDone = require('./lib/save-job-done')
   const saveJobError = require('./lib/save-job-error')
@@ -12,22 +29,6 @@ module.exports = (item, callback) => {
   const setupItem = require('./lib/setup-item')
   const setupSvarut = require('./lib/setup-svar-ut')
   const start = fromString(JSON.stringify(item))
-
-  const fromString = string => {
-    return miss.from(function (size, next) {
-      // if there's no more content
-      // left in the string, close the stream.
-      if (string.length <= 0) return next(null, null)
-
-      // Pull in a new chunk of text,
-      // removing it from the string.
-      var chunk = string.slice(0, size)
-      string = string.slice(size)
-
-      // Emit "chunk" from the stream.
-      next(null, chunk)
-    })
-  }
 
   const finished = error => {
     if (error) {
